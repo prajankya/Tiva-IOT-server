b37cea16166973ddc5c82e188ed523152db114f8
Prajankya <prajankya@gmail.com>
2016-11-29 13:03:21 +0530

    Updated server, added IP handler

---
+++ main.js
@@ -0,0 +1,76 @@
+const {
+    app,
+    ipcMain,
+    BrowserWindow
+} = require('electron')
+
+var express = require('express')
+var ex_app = express()
+var server = require('http').createServer(ex_app)
+var request = require('request')
+
+global.ips = []
+
+app.on('ready', () => {
+    // Create the browser window.
+    global.win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    global.win.loadURL(`file://${__dirname}/app/index.html`)
+
+    //win.webContents.openDevTools()
+
+    global.win.on('closed', () => {
+        global.win = null
+    })
+})
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (global.win === null) {
+        createWindow()
+    }
+})
+
+require('./server')(ex_app)
+
+
+ex_app.set('port', process.env.PORT || 3000);
+
+server.listen(ex_app.get('port'), function() {
+    console.log("Started server on port " + ex_app.get('port') + " on IP(s) :");
+
+    var os = require('os');
+    var ifaces = os.networkInterfaces();
+
+    Object.keys(ifaces).forEach(function(ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function(iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                console.log(iface.address);
+            }
+            ++alias;
+        });
+    });
+});
--- server.js
+++ server.js
@@ -1,53 +1,16 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var request = require('request');
-var ips = [];
+require('array.prototype.find')
 
-app.use("/add/:ip", function(req, res, next) {
-    console.log("Connected : " + req.params.ip);
-    ips.push(req.params.ip);
-    res.end("done");
-});
-
-app.use("/button/:no/:state", function(req, res, next) {
-    console.log("button : " + req.params.no + " is " + req.params.state);
-    console.log("From IP : " + req.connection.remoteAddress);
-    for (var i = 0; i < ips.length; i++) {
-        console.log("Sending to IP : " + ips[i]);
-        if (req.connection.remoteAddress != ips[i]) {
-            request('http://' + ips[i] + '/LED_' + req.params.no + '_' + req.params.state, function(error, response, body) {
-                //console.log(response);
-            });
+function server(ex_app) {
+    ex_app.get("/add/:ip", function(req, res, next) {
+        console.log("Connected : " + req.params.ip)
+        if (global.ips.find(function() {
+                return req.params.ip;
+            })) {
+            res.end("duplicate")
+        } else {
+            global.ips.push(req.params.ip)
+            res.end("done")
         }
-    }
-    res.end("done");
-});
-app.set('port', process.env.PORT || 3000);
-
-server.listen(app.get('port'), function() {
-    console.log("Started server on port " + app.get('port') + " on IP(s) :");
-
-    var os = require('os');
-    var ifaces = os.networkInterfaces();
-
-    Object.keys(ifaces).forEach(function(ifname) {
-        var alias = 0;
-
-        ifaces[ifname].forEach(function(iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
-                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-                return;
-            }
-
-            if (alias >= 1) {
-                // this single interface has multiple ipv4 addresses
-                console.log(ifname + ':' + alias, iface.address);
-            } else {
-                // this interface has only one ipv4 adress
-                console.log(ifname, iface.address);
-            }
-            ++alias;
-        });
     });
-});
+}
+module.exports = server
